d9a3a9b0b6c535db393d1c2ea9c5bdef77aa96b9
Zhao, Jun <zhaojunlucky@sina.com>
2019-05-17 16:20:32 +0800

    about

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module'
+import { PagesModule } from './pages/pages.module'
 
 
 @NgModule({
@@ -16,6 +17,7 @@ import { SharedModule } from './shared/shared.module'
     AppRoutingModule,
     SharedModule,
     BrowserAnimationsModule,
+    PagesModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
