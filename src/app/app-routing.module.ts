6d6d9bcfcea7450dbd02d84127661fe776cac627
Zhao, Jun <zhaojunlucky@sina.com>
2019-05-17 16:58:54 +0800

    tools

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { AboutComponent } from './pages/about/about.component'
+import { ToolsComponent } from './pages/tools/tools.component'
+
 
 const routes: Routes = [
-  { path: 'about', component: AboutComponent }
+  { path: 'about', component: AboutComponent },
+  { path: 'tools', component: ToolsComponent },
 ];
 
 @NgModule({
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AboutComponent } from './about/about.component';
+import { ToolsComponent } from './tools/tools.component';
 
 @NgModule({
-  declarations: [AboutComponent],
+  declarations: [AboutComponent, ToolsComponent],
   imports: [
     CommonModule
   ],
